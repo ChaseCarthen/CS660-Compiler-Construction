f05083ecbc9b4977774a1dfb18ecdfd596b1dd33
chase carthen <chasec2331@gmail.com>
2015-10-27 16:54:24 -0700

    AST Generator added

--- astgenerator.py
+++ astgenerator.py
@@ -1,15 +1,15 @@
 #Props to https://github.com/eliben/pycparser/blob/master/pycparser/_ast_gen.py
 import sys
 import re
+import pprint
+from string import Template
 
 class Generator(object):
 	def __init__(self,cfgfile):
-		print "The cake is a lie"
 		self.cfgfile = cfgfile
-		self.data = [(node,params,tac) for(node,params,tac) in self.generate() ]
+		self.data = [RawNode(node,params,tac) for(node,params,tac) in self.parse()]
 
-	def generate(self):
-		print "HERE"
+	def parse(self):
 		cfile = open(self.cfgfile,"r")
 		string = cfile.read()
 		string = re.sub(r'#.*','',string)
@@ -19,6 +19,17 @@ class Generator(object):
 			params = re.findall(r'\[.*\]',i)[0].replace(']','').replace('[','').split(',')
 			tac = re.findall(r"{[^{}]*}",i)[0].replace('}','').replace('{','').split()
 			yield node, params, tac
+
+	def generate(self, file=None):
+		""" Generates the code into file, an open file buffer.
+		"""
+		code = Template(_PROLOGUE_COMMENT).substitute(
+		    cfg_filename=self.cfgfile)
+		code += _PROLOGUE_CODE
+		for node in self.data:
+			code += str(node.generate()) + '\n\n\n'
+
+		file.write(code)	
 
 class RawNode(object):
 	def __init__(self, name, params, tac):
@@ -30,7 +41,8 @@ class RawNode(object):
 
 		for i in params:
 			clean = i.rstrip('*')
-			self.entries.append(clean)
+			if i is not '':
+				self.entries.append(clean)
 
 			if i.endswith('**'):
 				self.sequence.append(clean)
@@ -42,7 +54,9 @@ class RawNode(object):
 	def generate(self):
 		code = self.start()
 		code += '\n' + self.makeBabies()
+		#code += '\n' + self.generateTAC()
 		code += '\n' + self.generateAttributes()
+		return code
 
 	def start(self):
 		code = "class %s(Node):\n" % self.name
@@ -56,7 +70,7 @@ class RawNode(object):
 			slots = "'coord', '__weakref__'"
 			arglist = '(self, coord=None)'
 
-		code += "	__slots__ = (%s)\n" % slots
+		code += "	__slots__ = (%s)\n\n" % slots
 		code += "	def __init__%s:\n" %arglist
 
 		for name in self.entries + ['coord']:
@@ -64,36 +78,108 @@ class RawNode(object):
 
 		return code
 
-    def makeBabies(self):
-        code = '    def children(self):\n'
+	def makeBabies(self):
+	    code = '    def children(self):\n'
 
-        if self.entries:
-            code += '        nodelist = []\n'
+	    if self.entries:
+	        code += '        nodelist = []\n'
 
-            for child in self.child:
-                code += (
-                    '        if self.%(child)s is not None:' +
-                    ' nodelist.append(("%(child)s", self.%(child)s))\n') % (
-                        dict(child=child))
+	        for child in self.child:
+	            code += (
+	                '        if self.%(child)s is not None:' +
+	                ' nodelist.append(("%(child)s", self.%(child)s))\n') % (
+	                    dict(child=child))
 
-            for seq in self.sequence:
-                code += (
-                    '        for i, child in enumerate(self.%(child)s or []):\n'
-                    '            nodelist.append(("%(child)s[%%d]" %% i, child))\n') % (
-                        dict(child=sequence))
+	        for seq in self.sequence:
+	            code += (
+	                '        for i, child in enumerate(self.%(child)s or []):\n'
+	                '            nodelist.append(("%(child)s[%%d]" %% i, child))\n') % (
+	                    dict(child=sequence))
 
-            code += '        return tuple(nodelist)\n'
+	        code += '        return tuple(nodelist)\n'
+	    else:
+	        code += '        return ()\n'
+
+	    return code
+
+	def generateAttributes(self):
+		code = "    attr_names = (" + ''.join("%r, " % nm for nm in self.attribute) + ')'
+		return code	
+
+	def generateTAC(self, tac):
+		stuff = tac
+		return tac
+
+_PROLOGUE_COMMENT = \
+r'''#-----------------------------------------------------------------
+# ** DISCLAIMER **
+# This code was automatically generated from the file:
+# $cfg_filename
+#
+# AST Node classes:
+#  - Heavy Inspiration as drawn from the team developing pycparser
+#  - Heavily altered to handle Three Address code
+#  - Shut up Terence
+#
+#-----------------------------------------------------------------
+'''
+
+_PROLOGUE_CODE = r'''
+import sys
+
+class Node(object):
+    __slots__ = ()
+    """ Base class for AST nodes. Auto-Generated.
+    """
+
+    def children(self):
+        """ A sequence of all children that are Nodes
+        """
+        pass
+
+    def show(self, buf=sys.stdout, offset=0, attrnames=False, nodenames=False, showcoord=False, _my_node_name=None):
+        """ buf:
+                Open IO buffer into which the Node is printed.
+            offset:
+                Initial offset (amount of leading spaces)
+            attrnames:
+                True if you want to see the attribute names in
+                name=value pairs. False to only see the values.
+            nodenames:
+                True if you want to see the actual node names
+                within their parents.
+            showcoord:
+                Do you want the coordinates of each Node to be
+                displayed.
+        """
+        lead = ' ' * offset
+        if nodenames and _my_node_name is not None:
+            buf.write(lead + self.__class__.__name__+ ' <' + _my_node_name + '>: ')
         else:
-            code += '        return ()\n'
+            buf.write(lead + self.__class__.__name__+ ': ')
+        if self.attr_names:
+            if attrnames:
+                nvlist = [(n, getattr(self,n)) for n in self.attr_names]
+                attrstr = ', '.join('%s=%s' % nv for nv in nvlist)
+            else:
+                vlist = [getattr(self, n) for n in self.attr_names]
+                attrstr = ', '.join('%s' % v for v in vlist)
+            buf.write(attrstr)
+        if showcoord:
+            buf.write(' (at %s)' % self.coord)
+        buf.write('\n')
+        for (child_name, child) in self.children():
+            child.show(
+                buf,
+                offset=offset + 2,
+                attrnames=attrnames,
+                nodenames=nodenames,
+                showcoord=showcoord,
+                _my_node_name=child_name)
 
-        return code
-
-    def _gen_attr_names(self):
-        code = "    attr_names = (" + ''.join("%r, " % nm for nm in self.attribute) + ')'
-        return code	
-
+'''
 
 
 if __name__ == "__main__":
 	astgen = Generator("ast.cfg")
-	astgen.generate()
+	astgen.generate(open('ast.py', 'w'))

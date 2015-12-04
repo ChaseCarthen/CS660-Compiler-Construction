142e7c700b93bb232ed8f936832aca28573e5006
Chase Carthen <chasec2331@gmail.com>
2015-12-04 14:51:23 -0800

    Adding register notes

--- src/register_allocation.py
+++ src/register_allocation.py
@@ -1,12 +1,23 @@
-ignored = [0,1,2,3,4,5,6,7,26,27,28,29,30,31]
+ignored = [0,1,26,27]
 
 
 class RegisterAllocation(object):
-
+'''
+ 2-3 results $v0 - $v1
+ 4-7 arguments $a0-$a3
+ 8-15,24-25 temporaries $t0-$t9
+ 16-24 saved $s0-$s7
+ 28 global pointer $gp
+ 29 stack pointer $sp
+ 30 frame pointer $fp
+ 31 return address $ra
+'''
   def __init__(self):
     # Init all registers to empty
     # TODO: exempt the reserved registers from the list
     self.registers = {}
+    self.saved = {}
+    self.temporary = {}
     for i in range(0,32):
       if not i in ignored:
         self.registers[i] = None
